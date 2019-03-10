ef24705d2a6ceee299e343286427933497d74205
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-10 16:43:48 -0400

    Brew formula update for rsq version v0.2.0-beta.1

--- rsq.rb
+++ rsq.rb
@@ -1,9 +1,9 @@
 class Rsq < Formula
   desc ""
   homepage ""
-  url "https://github.com/metrumresearchgroup/rsq/releases/download/v0.1.0-rc.1/rsq_0.1.0-rc.1_darwin_amd64.tar.gz"
-  version "0.1.0-rc.1"
-  sha256 "b209eef405b43f6cc8ba434087873c179b5aea688e54517c43a087f0b817049e"
+  url "https://github.com/metrumresearchgroup/rsq/releases/download/v0.2.0-beta.1/rsq_0.2.0-beta.1_darwin_amd64.tar.gz"
+  version "0.2.0-beta.1"
+  sha256 "ad180bed454be611f23a73a4bf869208b9714db28362eaa470ee7cc25a0a4f26"
 
   def install
     bin.install "rsq"
