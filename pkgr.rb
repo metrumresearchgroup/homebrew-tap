7759f13fa5d08118398bf02432d1f6085f560b9d
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-10 11:35:36 -0400

    Brew formula update for pkgr version v0.3.0-rc.2

--- pkgr.rb
+++ pkgr.rb
@@ -1,9 +1,9 @@
 class Pkgr < Formula
   desc ""
   homepage ""
-  url "https://github.com/metrumresearchgroup/pkgr/releases/download/v0.3.0-rc.1/pkgr_0.3.0-rc.1_darwin_amd64.tar.gz"
-  version "0.3.0-rc.1"
-  sha256 "81d117f8b89cee1337a54126b63d1e1e31cafdbc6361511c20a0564e3ae960a5"
+  url "https://github.com/metrumresearchgroup/pkgr/releases/download/v0.3.0-rc.2/pkgr_0.3.0-rc.2_darwin_amd64.tar.gz"
+  version "0.3.0-rc.2"
+  sha256 "7bb70f0b69d9928355da33e9efeacaaa44cdd9eec10f7101b4f66111462c4b00"
 
   def install
     bin.install "pkgr"
