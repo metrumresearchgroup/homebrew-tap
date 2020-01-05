aecf5136baf19e1a3f2224f53d1cfb1bcd7ca8c4
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-04 22:39:21 -0500

    Brew formula update for bbi version v2.1.0-alpha.3

--- bbi.rb
+++ bbi.rb
@@ -2,16 +2,16 @@
 class Bbi < Formula
   desc ""
   homepage ""
-  version "2.1.0-alpha.2"
+  version "2.1.0-alpha.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metrumresearchgroup/babylon/releases/download/v2.1.0-alpha.2/bbi_2.1.0-alpha.2_darwin_amd64.tar.gz"
-    sha256 "c8b8ca2494a20e578d63ac5b6782238d29643be9dc07596acd5d3540026a3b94"
+    url "https://github.com/metrumresearchgroup/babylon/releases/download/v2.1.0-alpha.3/bbi_2.1.0-alpha.3_darwin_amd64.tar.gz"
+    sha256 "eee98f6be1c06fadee941fb500e1f8c595339f0cd90f6fb060a699858eaaa7d4"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/metrumresearchgroup/babylon/releases/download/v2.1.0-alpha.2/bbi_2.1.0-alpha.2_linux_amd64.tar.gz"
-      sha256 "5835baa3372d845d29ca048f48ef89c4a82c1716a22437d5fb6976dc912aa91a"
+      url "https://github.com/metrumresearchgroup/babylon/releases/download/v2.1.0-alpha.3/bbi_2.1.0-alpha.3_linux_amd64.tar.gz"
+      sha256 "7c4631d31b5f63f15556a9a89cd1fd4dafd5d1d5fd0f16c3318f896aca17574c"
     end
   end
 
