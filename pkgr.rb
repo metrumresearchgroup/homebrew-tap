be306330beb07cf46fcd750198dd492a9edeee73
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-25 22:39:57 -0400

    Brew formula update for pkgr version v1.1.0

--- pkgr.rb
+++ pkgr.rb
@@ -2,16 +2,16 @@
 class Pkgr < Formula
   desc ""
   homepage ""
-  version "1.0.2"
+  version "1.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v1.0.2/pkgr_1.0.2_darwin_amd64.tar.gz"
-    sha256 "f1789a7006a7ee8b58e3bae87e821422abf0800786da96afeab748afde5dbd3e"
+    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v1.1.0/pkgr_1.1.0_darwin_amd64.tar.gz"
+    sha256 "507ebfc4de74d8b984682254c14fa8584203d076137a78ce6bfee696cbb28d6c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/metrumresearchgroup/pkgr/releases/download/v1.0.2/pkgr_1.0.2_linux_amd64.tar.gz"
-      sha256 "c45075553f41843fdd7534b2bc2443cedc2b81b97c8afc754b6f115e06f74cfa"
+      url "https://github.com/metrumresearchgroup/pkgr/releases/download/v1.1.0/pkgr_1.1.0_linux_amd64.tar.gz"
+      sha256 "4ba81d435593e0f5cb58bef72c3199d818e0673014d2de7e740277ac576c14d6"
     end
   end
 
