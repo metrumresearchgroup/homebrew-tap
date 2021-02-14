f78091a55920ee01981570b9e6fc65e10c8ca077
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-14 15:35:54 -0500

    Brew formula update for pkgr version v3.0.0-alpha.2

--- pkgr.rb
+++ pkgr.rb
@@ -2,16 +2,16 @@
 class Pkgr < Formula
   desc ""
   homepage ""
-  version "3.0.0-alpha.1"
+  version "3.0.0-alpha.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v3.0.0-alpha.1/pkgr_3.0.0-alpha.1_darwin_amd64.tar.gz"
-    sha256 "93dc269f599b771b1a305d1071e93e6d35c1062726c7c822eadbb852ba1056cc"
+    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v3.0.0-alpha.2/pkgr_3.0.0-alpha.2_darwin_amd64.tar.gz"
+    sha256 "005216f0ecbd1a7b9a7d9c8995a10587f7f5457f32bc9e942bd6145fea23f7db"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v3.0.0-alpha.1/pkgr_3.0.0-alpha.1_linux_amd64.tar.gz"
-    sha256 "36810c142f625c10c02fbf482589ccbf631fac8e44025a374d2c1699ae04d7cb"
+    url "https://github.com/metrumresearchgroup/pkgr/releases/download/v3.0.0-alpha.2/pkgr_3.0.0-alpha.2_linux_amd64.tar.gz"
+    sha256 "f2f13b1bb812d2b87f9d07fa0b1f753c1809ac5db9c0f8649d66f49bbd32bfe3"
   end
 
   def install
